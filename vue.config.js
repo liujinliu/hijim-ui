f7a833eaa716a06a33f140ce42acf7a218ef629d
gary <514371547@qq.com>
2022-09-27 22:42:16 +0800

    add dev server config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave: true,
+  devServer: {
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: `http://${process.env.VUE_APP_DEV_PROXY_IP}:${process.env.VUE_APP_DEV_PROXY_PORT}`,
+        ws: false,
+        changeOrigin: true
+      }
+    }
+  }
 })
